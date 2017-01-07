54c2496e7becd41032765a0323af6f2bfbfd11cd
justinborromeo <jborrome@edu.uwaterloo.ca>
2017-01-07 13:16:01 -0500

    Initial Commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+function onstart(){
+	if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+		window.open("mobilelanding.html", "_self");
+	}else{
+		
+	}
+
+}
---
+++ js/main.js
@@ -0,0 +1,74 @@
+index=1;
+
+function register() {
+	window.open("https://docs.google.com/forms/d/e/1FAIpQLSfcYpR6AheO2dgQAW0D1CrW8yor6RlQE95S56XKcoOPFjOR_g/viewform?c=0&w=1&usp=mail_form_link", "_blank")
+}
+
+function openMain() {
+	window.open("main.html", "_self");
+}
+
+function hideAll(){
+	$("#menu").hide();
+	$("#page1").hide();
+	$("#page2").hide();
+	$("#page3").hide();
+	$("#page4").hide();
+}
+
+function begin(){
+	openOne();
+	$("#hider").hide();
+}
+function openOne(){
+	hideAll();
+	$("#menu").show();
+	$("#page1").fadeIn(1000);
+	index=1;
+}
+
+function openTwo(){
+	hideAll();
+	$("#menu").show();
+	$("#page2").fadeIn(1000);
+	index=2;
+}
+
+function openThree(){
+	hideAll();
+	$("#menu").show();
+	$("#page3").fadeIn(1000);
+	index=3;
+}
+
+document.onkeydown = checkKey;
+
+function checkKey(e) {
+
+    e = e || window.event;
+    if (e.keyCode == '37') {
+       // left arrow
+       if((index-1)<1){
+
+       }else{
+     	    index--;
+	        hideAll();
+	     	$("#menu").show();
+
+			$("#page"+index).fadeIn(1000);
+		}
+    }
+    else if (e.keyCode == '39') {
+       // right arrow
+       if((index+1)>7){
+
+       }else{
+     	    index++;
+	        hideAll();
+	     	$("#menu").show();
+
+			$("#page"+index).fadeIn(1000);
+		}
+    }
+
+}
