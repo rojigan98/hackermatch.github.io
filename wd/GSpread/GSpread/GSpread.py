bcd3e67d782aa9cbf1497eb4a231278943fbbf89
rojigan98 <rojigan98@gmail.com>
2017-01-07 21:44:05 -0500

    NOT TESTED New function: final_team_recommendation

--- wd/GSpread/GSpread/GSpread.py
+++ wd/GSpread/GSpread/GSpread.py
@@ -87,6 +87,36 @@ def get_positions(worksheet):
     
     return new_dict
 
+def final_team_recommendation(dictionary_with_positions, dictionary, user_data):
+    ''' NOT TESTED NOT TESTED Input: First input is a dictionary where the name is the key and the associated score is the value. The second input is the position that the user selected on his Google Form. More specifically a dictionary where the key is the person's email and the associated value is the score. Output: A final team recommendation as a list.'''
+    sorted_list = sorting_by_score(dictionary)
+    user_position = user_data[9]
+    final_team = [sorted_list[0][1]]
+    if user_position == final_team:
+        same_modifier = True
+    else:
+        same_modifier = False
+    
+    if same_modifier == True:
+        people_found = 2
+        for i in range(2, len(sorted_list)):
+            if dictionary_with_positions[sorted_list[i][1]] != user_position:
+                final_team.append(sorted_list[i][1])
+                people_found += 1
+            if people_found == 4:
+                return final_team
+        return final_team
+    else:
+        final_team.append(sorted_list[2][1])
+        excess_position = dictionary_with_positions[sorted_list[2][1]]
+        for i in range(3, len(sorted_list)):
+            if dictionary_with_positions[sorted_list[i][1]] != excess_position:
+                final_team.append(sorted_list[i][1])
+                return final_team
+        return final_team
+
+
+
 
 def match(email):
     '''(str) -> list of str
