a719f28e3bbb12692737ec8184e444519760e921
Sadi Muhammad Wali <sadiwali@hotmail.com>
2017-01-07 19:18:49 -0500

    update

--- wd/GSpread/GSpread/GSpread.py
+++ wd/GSpread/GSpread/GSpread.py
@@ -25,18 +25,9 @@ ws = gc.open_by_url(file_url)
 # open the worksheet
 worksheet = ws.get_worksheet(0)
 
-# get matches
-match('sadiw@gmail.com')
 
 
 
-<<<<<<< HEAD
-=======
-
-    
-    
-   
->>>>>>> origin/master
 def get_all_rows(worksheet):
     '''Input: A worksheet file, Output: A list of lists, each list is a row of data from the sheet
         This function takes in a worksheet and outputs every row as an element of a list. (Each one of these elements is also a list)'''
@@ -49,7 +40,8 @@ def get_all_rows(worksheet):
 
 
 def calc_match(listA, listB):
-    '''Input: Two lists, A and B, list A is a list of person A's reasons for doing the hackathon, similarly for list B. Output: A score out of 1, of how well the two people match in this category'''
+    '''Input: Two lists, A and B, list A is a list of person A's reasons for doing the hackathon,
+    similarly for list B. Output: A score out of 1, of how well the two people match in this category'''
     listC = set(listA + listB)
     total = len(listA) + len(listB)
     if total == 0:
@@ -58,7 +50,7 @@ def calc_match(listA, listB):
     for val in listC:
         if (val in listA) and (val in listB):
             sum += 2
-    
+
     return (sum/total)
 
 
@@ -74,8 +66,9 @@ def match(email):
     '''
 
     # row index of the checking user
-    user_ind = worksheet.col_values(reason_index).index(email)
-    
+    user_ind = worksheet.col_values(email_index).index(email) + 1
+
+
     # entries not zero based
     num_entries = worksheet.row_count+1
 
@@ -85,6 +78,7 @@ def match(email):
     # list of reasons for checking user
     c_user_row = worksheet.row_values(user_ind)
     c_user_list = c_user_row[reason_index-1].split(',') # list of reasons
+
     c_user_list_A = [c_user_row[type_index-1]] # list of type
 
     # loop through each row, excluding the row of user
@@ -98,8 +92,15 @@ def match(email):
             p_user_list = user_row[reason_index-1].split(',') # list of reason for potential user
             p_user_list_A = [user_row[type_index-1]] # list of type for potential user
             # add email of potential user and score
-            user_score[user_row[email_index-1]] = calc_match(c_user_list,p_user_list) * reason_multiplierd + calc_match(c_user_list_A,p_user_list_A) * type_multiplier
+            reason_score = 0
+            type_score = 0
+            print('pass')
+            # input(str(c_user_list) + ' and p is: '+ str(p_user_list))
+    
+            reason_score = calc_match(c_user_list,p_user_list);
 
+            user_score[user_row[email_index-1]] = calc_match(c_user_list,p_user_list) * reason_multiplier + calc_match(c_user_list_A,p_user_list_A) * type_multiplier
+            print('pass')
     # user_score is dictionary containing scores. 
     # calculate interest score
 
@@ -110,13 +111,7 @@ def match(email):
     
 
 
-
-
-
-
-
-
-
     
     
-    
+# get matches
+match('sadiw@gmail.com')
